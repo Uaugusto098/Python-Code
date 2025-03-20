1114772bef009e37856bb556b7083a1e20f61fb9
Pedro augusto borges de sá <Pedroaugusto.borges36@gmail.com>
2025-03-19 22:10:16 -0300

    Calculadora em python

---
+++ Calculadora.py
@@ -0,0 +1,59 @@
+print(" Calculadora em Python")
+
+
+print("\n")
+
+
+
+c=(input("Digite qual operação realizar: "))
+a=float(input("\nDigite o primeiro numero: "))
+b=float(input("\nDigite o segundo numero: "))
+
+
+if c=="+":
+    
+    d=a+b
+    
+    print ("\nO resultado da soma é de: ",d)
+    
+elif c=="-":
+    
+    d=a-b
+    
+    print ("\nO resultado da subtração é de: ",d)
+    
+elif c=="*":
+    
+    d=a*b
+    
+    print ("\nO resultado da multiplicação é de: ",d)
+    
+    
+elif c=="/":
+    
+   d=a/b
+    
+    
+    
+   print ("\nO resultado da divisão é de: ",d)
+   
+   
+else:
+    print("\nDigite uma operação existente")
+    
+    
+
+    
+
+    
+
+    
+
+
+
+
+
+
+
+
+
