6d1a3d74169fa3100b5e1633b3bee64f9adc0af1
Pedro <pedroaugusto.borges36@gmail.com>
2025-04-12 11:40:11 -0300

    Atualização no código(a terminar)

--- aula-loops.py
+++ aula-loops.py
@@ -1,2 +1,36 @@
-figura_publica = 'figura publica'
-print (figura_publica)
+print("---Sistema de notas de alunos para professores---")
+notas=[]
+nomes=[]
+
+
+
+
+log=input("Digite o usuario: ")
+senha=input("Digite sua senha: ")
+
+
+while log!="Pedro" and senha!="prof123":
+
+    for i in range(1,4):
+        print(f"Tentivas: {i}")
+        log=input("\nDigite o usuario novamente: ")
+        senha=input("\nDigite a senha novamente: ")
+
+    
+    print("\nConta bloqueada")
+    break 
+
+
+
+
+
+if log =="Pedro" and senha =="prof123":
+
+    for l in range(1,4):
+        nota=int(input("Digite as notas:  "))
+        notas.append[nota]
+
+
+
+
+
